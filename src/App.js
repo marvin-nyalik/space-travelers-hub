c761520bac811462b0e595e263d384159afd3a1c
MARVIN <o.silverygold@gmail.com>
2023-07-24 14:31:50 +0300

    Update basic configuration and NPM dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Missions from './components/Missions';
+import Profile from './components/Profile';
+import Rockets from './components/Rockets';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={(
+              <Layout>
+                {' '}
+                <Rockets />
+                {' '}
+              </Layout>
+)}
+          />
+          <Route
+            path="/missions"
+            element={(
+              <Layout>
+                {' '}
+                <Missions />
+                {' '}
+              </Layout>
+)}
+          />
+          <Route
+            path="/profile"
+            element={(
+              <Layout>
+                {' '}
+                <Profile />
+                {' '}
+              </Layout>
+)}
+          />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
